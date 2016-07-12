19613e17f652c0d512d61eef02b738a25b185e37
joshjirjis <josh.j.jirjis@gmail.com>
2016-07-12 16:33:34 -0500

    Added Map

--- Places/ViewController.swift
+++ Places/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
