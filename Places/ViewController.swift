ef059344cabbf06110b6c72b5542e75ba170f8ee
joshjirjis <josh.j.jirjis@gmail.com>
2016-07-12 12:16:56 -0500

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Josh Jirjis on 7/12/16.
+//  Copyright © 2016 Jirjis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
